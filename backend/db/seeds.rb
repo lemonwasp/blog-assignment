5375dfeb14665a10afc4689f170d98cb6b5c6e74
htg2001gd141@gmail.com <htg2001gd141@gmail.com>
2025-03-14 10:41:27 +0900

    Added category&tag seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# カテゴリー追加
+categories = ["技術 & プログラミング", "ライフスタイル", "ビジネス & 経済", "健康 & ウェルネス", "レビュー & おすすめ"]
+
+categories.each { |category| Category.find_or_create_by(name: category) }
+
+
+# タグ追加
+tags = ["AI & 機械学習", "セキュリティ", "自動化", "Web開発", "クラウドコンピューティング", "旅行記", "趣味", "自己啓発", 
+        "フリーランス", "ミニマリズム", "スタートアップ", "投資戦略", "マーケティング", "サイドプロジェクト", "生産性向上", 
+        "運動ルーティン", "ダイエット", "メンタルケア", "本のレビュー", "映画 & ドラマ"]
+
+tags.each { |tag| Tag.find_or_create_by(name: tag) }
